b811eadaf81e44138f71f489b96f0d8695f9d494
Miguel Stoppa <m.stoppa@campus.unimib.it>
2019-10-25 17:17:08 +0200

    Aggiunta nuovamente una stampa

--- src/gitExercise2/main/HelloGit.java
+++ src/gitExercise2/main/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 	public static void main (String [] args) {
 		System.out.println("Questa è una nuovissima stampa, che serve per simulare la modifica simultanea di una riga di codice!");
 		System.out.println("è stata aggiunta un'altra stampa2");
+		System.out.println("Aggiunta altra stampa");
 	}
 }
